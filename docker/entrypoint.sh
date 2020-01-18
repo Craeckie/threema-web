1a103e4397c2f3a19a1edb634eec9028a9aade22
Craeckie <josia-login@rosskopfs.de>
2020-01-18 14:43:04 +0100

    Fixed entrypoint.sh
    Env variable SALTYRTC_HOST couldn't be changed.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 echo "Patching config file..."
 cd /usr/share/nginx/html/
 if [ ! -z "$SALTYRTC_HOST" ]; then
-    sed -i -E "s/SALTYRTC_HOST:\s*null,/SALTYRTC_HOST:'${SALTYRTC_HOST}',/g" *.bundle.js
+    sed -i -E "s/SALTYRTC_HOST:[^,]+,/SALTYRTC_HOST:'${SALTYRTC_HOST}',/g" *.bundle.js
 fi
 sed -i -E "s/SALTYRTC_PORT:\s*[^,]*,/SALTYRTC_PORT:${SALTYRTC_PORT},/g" *.bundle.js
 sed -i -E "s/SALTYRTC_SERVER_KEY:\s*\"[^\"]*\",/SALTYRTC_SERVER_KEY:\"${SALTYRTC_SERVER_KEY}\",/g" *.bundle.js
